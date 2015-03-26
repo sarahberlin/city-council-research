071af4ea87ba789b2697ebea3b76163b073de19d
sarahberlin <sarah.d.berlin@gmail.com>
2015-03-26 15:21:22 -0400

    Update new-york-city.py

--- new-york-city.py
+++ new-york-city.py
@@ -14,12 +14,13 @@ def get_page_urls():
     return [a.attrs.get('href') for a in soup.select('tr a[href^=/d]')]
 
 #get data from each individual councilor's page
-
 def get_councilor_data(page_url):
     councilor_data = {}
     response = requests.get(root_url + page_url)
     soup = bs4.BeautifulSoup(response.text)
+    #district
     councilor_data['district'] = ((soup.select('title')[0].get_text()).split(" - ")[0]).encode('utf-8')
+    #name
     try:
         if page_url == '/d6/html/members/home.shtml':
             councilor_data['name'] = soup.select('tr td em')[0].get_text().encode('utf-8').split(' - ')[0]
@@ -27,6 +28,7 @@ def get_councilor_data(page_url):
             councilor_data['name'] = soup.select('h1')[0].get_text().encode('utf-8').replace("\n", "").replace('"', '')
     except:
         pass
+    #party
     try:
         if len(soup.select('title')[0].get_text().split(" - "))==2:
             councilor_data['party'] = "Unknown"
@@ -34,19 +36,25 @@ def get_councilor_data(page_url):
             councilor_data['party'] = ((soup.select('title')[0].get_text()).split(" - ")[2]).encode('utf-8')
     except:
         pass  
+    #phone
     try:      
         if len(soup.select('td.nav_text br')[2].get_text().split("District")[0].split("District")[0].encode('utf-8').replace("\n", "").replace("            ", "")) > 15:
                 councilor_data['phone'] = ''
         else:    
-            councilor_data['phone'] = soup.select('td.nav_text br')[2].get_text().split("District")[0].split("District")[0].encode('utf-8').replace("\n", "").replace("            ", "")
+            councilor_data['phone'] = soup.select('td.nav_text br')[2].get_text().split("District")[0].split("District")[0].encode('utf-8').replace("\n", "").replace("            ", "").strip()
     except:
         pass
+    #email
     try:
         councilor_data['email'] = ([a.attrs.get('href') for a in soup.select('tr td a[href^=mailto]')][0]).replace('mailto:', '')
     except:
         pass              
+    #website
     councilor_data['website'] = (root_url + page_url).encode('utf-8')
-    councilor_data['address'] = "City Hall Park, New York, NY 10007"           
+    #address
+    str = ' '
+    seq = soup.find('td', {'class': 'nav_text'}).get_text().encode('utf-8').split('\n')[1].replace('            ',''),soup.find('td', {'class': 'nav_text'}).get_text().encode('utf-8').split('\n')[2], soup.find('td', {'class': 'nav_text'}).get_text().encode('utf-8').replace('District Office Phone', '').replace('(Entrance on Hoffman Street)', '').split('\n')[3]
+    councilor_data['address'] = str.join(seq).strip()
     return councilor_data 
 
 
@@ -65,21 +73,10 @@ csvwriter = csv.DictWriter(ny_council_file, delimiter=',', fieldnames=fieldnames
 csvwriter.writerow(dict((fn,fn) for fn in fieldnames))
 for row in dictList:
     csvwriter.writerow(row)
+
 ny_council_file.close()
  
 with open("ny_council.csv", "r") as ny_council_csv:
      ny_council = ny_council_csv.read()
 
-print ny_council 
-
-
-
-
-
-
-
-
-
-
-
-
+print ny_council
